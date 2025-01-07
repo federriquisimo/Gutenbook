99979026f1169fb1aba771e3c2f4a60e06a7c5c1
federriquisimo <federicoarias.engineering@gmail.com>
2025-01-07 11:27:02 -0500

    Se crean los archivos con las querys en el folder de reposityory

---
+++ src/main/java/com/icaro/gutenbook/repository/LibroRepository.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.icaro.gutenbook.repository;
+
+
+import com.icaro.gutenbook.model.Idioma;
+import com.icaro.gutenbook.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LibroRepository extends JpaRepository<Libro,Long> {
+    @Query("SELECT l FROM Libro l WHERE LOWER(l.titulo) LIKE LOWER(:nombre)")
+    Optional<Libro> obtenerLibroPorNombre(String nombre);
+
+    @Query("SELECT l FROM Libro l WHERE l.idioma=:idioma")
+    List<Libro> obtenerLibrosPorIdioma(Idioma idioma);
+}
